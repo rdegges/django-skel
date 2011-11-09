36ed0e63acdce66c8a9536d52bc36a52c48d94dd
Randall Degges <rdegges@gmail.com>
2011-11-09 15:19:15 -0800

    Enabling the admin via INSTALLED_APPS.

--- skel/settings.py
+++ skel/settings.py
@@ -115,10 +115,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+
+    # Admin panel and documentation:
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
