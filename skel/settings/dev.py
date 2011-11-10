923344449ffb64dcc87c50d523dbe8c188095daa
Randall Degges <rdegges@gmail.com>
2011-11-09 17:17:45 -0800

    Adding settings.dev module, for local development.
    This is what you'll primarily be using, it provides sane local configurations,
    including a sqlite db, and local memory caching.

---
+++ skel/settings/dev.py
@@ -0,0 +1,46 @@
+"""Development settings and globals."""
+
+
+from os.path import join, normpath
+
+from common import *
+
+
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#debug
+DEBUG = True
+
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+########## END EMAIL CONFIGURATION
+
+
+########## DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': normpath(join(DJANGO_ROOT, 'default.db')),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+########## DATABASE CONFIGURATION
+
+
+########## CACHE CONFIGURATION
+# See: https://docs.djangoproject.com/en/1.3/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+########## END CACHE CONFIGURATION
