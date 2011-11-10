e18432b7d4f314e4b5ddfa2ead3142466936172d
Randall Degges <rdegges@gmail.com>
2011-11-10 01:28:11 -0800

    Adding ``migrate`` task.

--- fabfile.py
+++ fabfile.py
@@ -14,3 +14,17 @@ env.settings = 'settings.prod'
 def syncdb():
     """Run a syncdb."""
     local('%(run)s syncdb --noinput --settings=%(settings)s' % env)
+
+
+@task
+def migrate(app=None):
+    """Apply one (or more) migrations. If no app is specified, fabric will
+    attempt to run a site-wide migration.
+
+    :param str app: Django app name to migrate.
+    """
+    if app:
+        local('%s migrate %s --noinput --settings=%s' % (env.run, app,
+                env.settings))
+    else:
+        local('%(run)s migrate --noinput --settings=%(settings)s' % env)
