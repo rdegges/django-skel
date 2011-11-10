a43ab00581a396a037b3f935818ae9de96a32e37
Randall Degges <rdegges@gmail.com>
2011-11-09 23:58:29 -0800

    Implementing ``env`` lambda as a helper.

--- skel/settings/prod.py
+++ skel/settings/prod.py
@@ -8,7 +8,9 @@ from urlparse import urlparse, uses_netloc
 from common import *
 
 
+# Helper lambda for gracefully degrading environmental options:
 env = lambda e, d: environ[e] if environ.has_key(e) else d
+
 
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-backend
@@ -18,13 +20,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST', 'smtp.gmail.com')
 
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = environ['EMAIL_HOST_PASSWORD'] or ''
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', '')
 
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-host-user
-EMAIL_HOST_USER = environ['EMAIL_HOST_USER'] or 'your_email@example.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', 'your_email@example.com')
 
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-port
-EMAIL_PORT = environ['EMAIL_PORT'] or 587
+EMAIL_PORT = env('EMAIL_PORT', 587)
 
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
