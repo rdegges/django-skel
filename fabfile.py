dcfdf84a3b9fef831626d9585028a76c8a875504
Randall Degges <rdegges@gmail.com>
2011-11-10 01:12:24 -0800

    Adding task ``syncdb``.

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,16 @@
 """Management utilities."""
 
 
-from os.path import abspath
-
 from fabric.api import env, local, task
-from fabric.context_managers import lcd
 
 
 ########## GLOBALS
-env.root = abspath('skel')
-env.admin = 'django-admin.py'
+env.run = 'heroku run python skel/manage.py'
+env.settings = 'settings.prod'
 ########## END GLOBALS
 
 
 @task
 def syncdb():
     """Run a syncdb."""
-    with lcd(env.root):
-        local('%(admin)s help' % env)
-        local('%(admin)s syncdb --noinput' % env)
+    local('%(run)s syncdb --noinput --settings=%(settings)s' % env)
