a4849f62348535d2beb9fa91566e0d0277ae917d
Randall Degges <rdegges@gmail.com>
2011-11-12 11:05:10 -0800

    Adding HELPERS section, with ``cont`` helper.
    This function is a wrapper that will allow me to gracefully handle error-prone
    commands.

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,39 @@
 """Management utilities."""
 
 
-from fabric.api import env, local, task
+from fabric.contrib.console import confirm
+from fabric.api import abort, env, local, settings, task
 
 
 ########## GLOBALS
 env.run = 'heroku run python skel/manage.py'
 env.settings = 'settings.prod'
 ########## END GLOBALS
+
+
+########## HELPERS
+def cont(cmd, message):
+    """Given a command, ``cmd``, and a message, ``message``, allow a user to
+    either continue or break execution if errors occur while executing ``cmd``.
+
+    :param str cmd: The command to execute on the local system.
+    :param str message: The message to display to the user on failure.
+
+    .. note::
+        ``message`` should be phrased in the form of a question, as if ``cmd``'s
+        execution fails, we'll ask the user to press 'y' or 'n' to continue or
+        cancel exeuction, respectively.
+
+    Usage::
+
+        cont('heroku run ...', "Couldn't complete %s. Continue anyway?" % cmd)
+    """
+    with settings(warn_only=True):
+        result = local(cmd, capture=True)
+
+    if message and result.failed and not confirm(message):
+        abort('Stopped execution per user request.')
+########## END HELPERS
 
 
 ########## DATABASE MANAGEMENT
