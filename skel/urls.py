7b65f4667c32436e5631698d5f2a30fbebf00a20
Randall Degges <rdegges@gmail.com>
2012-03-10 14:34:25 -0800

    Adding documentation reference for admin configuration.

--- skel/urls.py
+++ skel/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
 
 
+# See: https://docs.djangoproject.com/en/1.3/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
