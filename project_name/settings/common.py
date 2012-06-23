ed6480eb76ef8227e2c15d89b541a54ba4097ff5
Randall Degges <rdegges@gmail.com>
2012-06-23 15:01:57 -0700

    Updating documentation link in settings.common.

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -96,7 +96,7 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#admin-media-prefix
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
-# See: https://docs.djangoproject.com/en/1.3/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
     normpath(join(DJANGO_ROOT, 'assets')),
 )
