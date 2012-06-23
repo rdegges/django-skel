05bc91568ef0ccb12503915ec14ff1662dd4d160
Randall Degges <rdegges@gmail.com>
2012-06-23 16:57:36 -0700

    Adding django-debug-toolbar configuration to settings.dev.
    This by default allows you to use the nice debug toolbar while you're
    developing locally (only accessible via localhost).

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -50,3 +50,19 @@ CACHES = {
 # See: http://docs.celeryq.org/en/latest/configuration.html#celery-always-eager
 CELERY_ALWAYS_EAGER = True
 ########## END CELERY CONFIGURATION
+
+
+########## TOOLBAR CONFIGURATION
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INTERNAL_IPS = ('127.0.0.1',)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+########## END TOOLBAR CONFIGURATION
