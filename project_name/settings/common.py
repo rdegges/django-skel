44ba127cf4778fef39667a309bb0e064cbd61263
Randall Degges <rdegges@gmail.com>
2012-06-23 15:01:18 -0700

    Updating our settings.common documentation links.
    They now point to the development docs, which is better than hardcoding the
    release version.

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -25,27 +25,27 @@ path.append(DJANGO_ROOT)
 
 
 ########## DEBUG CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#debug
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = False
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#template-debug
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
 TEMPLATE_DEBUG = DEBUG
 ########## END DEBUG CONFIGURATION
 
 
 ########## MANAGER CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#admins
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
     ('Your Name', 'your_email@example.com'),
 )
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#managers
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 ########## END MANAGER CONFIGURATION
 
 
 ########## DATABASE CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#databases
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.',
@@ -60,37 +60,37 @@ DATABASES = {
 
 
 ########## GENERAL CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#time-zone
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
 TIME_ZONE = 'America/Los_Angeles'
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#language-code
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en-us'
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#site-id
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#use-i18n
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
 USE_I18N = True
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#use-l10n
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
 USE_L10N = True
 ########## END GENERAL CONFIGURATION
 
 
 ########## MEDIA CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#media-root
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-root
 MEDIA_ROOT = normpath(join(DJANGO_ROOT, 'media'))
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#media-url
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = '/media/'
 ########## END MEDIA CONFIGURATION
 
 
 ########## STATIC FILE CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#static-root
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = normpath(join(DJANGO_ROOT, 'static'))
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#static-url
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/1.3/ref/settings/#admin-media-prefix
@@ -101,7 +101,7 @@ STATICFILES_DIRS = (
     normpath(join(DJANGO_ROOT, 'assets')),
 )
 
-# See: https://docs.djangoproject.com/en/1.3/ref/contrib/staticfiles/#staticfiles-finders
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -110,13 +110,13 @@ STATICFILES_FINDERS = (
 
 
 ########## SECRET CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#secret-key
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = r"{{ secret_key }}"
 ########## END SECRET CONFIGURATION
 
 
 ########## FIXTURE CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#std:setting-FIXTURE_DIRS
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-FIXTURE_DIRS
 FIXTURE_DIRS = (
     normpath(join(DJANGO_ROOT, 'fixtures')),
 )
@@ -124,7 +124,7 @@ FIXTURE_DIRS = (
 
 
 ########## TEMPLATE CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#template-context-processors
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
@@ -135,13 +135,13 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.messages.context_processors.messages'
 )
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#template-loaders
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#template-dirs
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
 TEMPLATE_DIRS = (
     normpath(join(DJANGO_ROOT, 'templates')),
 )
@@ -149,7 +149,7 @@ TEMPLATE_DIRS = (
 
 
 ########## MIDDLEWARE CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#middleware-classes
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#middleware-classes
 MIDDLEWARE_CLASSES = (
     # Use GZip compression to reduce bandwidth.
     'django.middleware.gzip.GZipMiddleware',
@@ -168,7 +168,7 @@ MIDDLEWARE_CLASSES = (
 
 
 ########## URL CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#root-urlconf
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
 ROOT_URLCONF = '%s.urls' % SITE_NAME
 ########## END URL CONFIGURATION
 
@@ -201,13 +201,13 @@ THIRD_PARTY_APPS = (
 LOCAL_APPS = (
 )
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#installed-apps
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 ########## END APP CONFIGURATION
 
 
 ########## LOGGING CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#logging
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -232,9 +232,12 @@ LOGGING = {
 # See: http://celery.readthedocs.org/en/latest/configuration.html#celery-task-result-expires
 CELERY_TASK_RESULT_EXPIRES = timedelta(minutes=30)
 
-# See: http://ask.github.com/django-celery/
+# See: http://celery.github.com/celery/django/
 setup_loader()
 ########## END CELERY CONFIGURATION
 
-# Python dotted path to the WSGI application used by Django's runserver.
+
+########## WSGI CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
 WSGI_APPLICATION = 'wsgi.application'
+########## END WSGI CONFIGURATION
