1b8fcb1ff4919fbe815ba6e2a2afded896e48a43
Randall Degges <rdegges@gmail.com>
2012-06-24 16:28:29 -0700

    Updating documentation links in our root urlconf.

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
 
 
-# See: https://docs.djangoproject.com/en/1.3/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
 
 
-# See: https://docs.djangoproject.com/en/1.3/topics/http/urls/
+# See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
     # Admin panel and documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
