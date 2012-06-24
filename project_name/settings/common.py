f83da4f6771407b88264b7431b8b529a45a9a6e7
Randall Degges <rdegges@gmail.com>
2012-06-23 23:12:35 -0700

    Fixing typo.

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -129,7 +129,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages'
+    'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
 )
 
