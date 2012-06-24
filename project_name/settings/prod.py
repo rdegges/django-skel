6053cdf074e319bcbf22504327972a0299d43bb2
Randall Degges <rdegges@gmail.com>
2012-06-24 16:16:06 -0700

    Updating BROKER_POOL_LIMIT to make use of the CloudAMQP free tier.
    Also added some useful information on calculating the pool number.

--- project_name/settings/prod.py
+++ project_name/settings/prod.py
@@ -52,6 +52,18 @@ CACHES = memcacheify()
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-transport
 BROKER_TRANSPORT = 'amqplib'
 
+# Set this number to the amount of allowed concurrent connections on your AMQP
+# provider, divided by the amount of active workers you have.
+#
+# For example, if you have the 'Little Lemur' CloudAMQP plan (their free tier),
+# they allow 3 concurrent connections. So if you run a single worker, you'd
+# want this number to be 3. If you had 3 workers running, you'd lower this
+# number to 1, since 3 workers each maintaining one open connection = 3
+# connections total.
+#
+# See: http://docs.celeryproject.org/en/latest/configuration.html#broker-pool-limit
+BROKER_POOL_LIMIT = 3
+
 # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-url
 BROKER_URL = environ.get('RABBITMQ_URL') or environ.get('CLOUDAMQP_URL')
 
