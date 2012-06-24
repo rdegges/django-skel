820c38c2400fff7d2f58cc1c0df408ebc333c176
Randall Degges <rdegges@gmail.com>
2012-06-23 22:20:58 -0700

    Updating documentation links in settings.prod module.

--- project_name/settings/prod.py
+++ project_name/settings/prod.py
@@ -11,28 +11,28 @@ from common import *
 
 
 ########## EMAIL CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-backend
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-host
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
 EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.gmail.com')
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-host-password
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
 EMAIL_HOST_PASSWORD = environ.get('EMAIL_HOST_PASSWORD', '')
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-host-user
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
 EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'your_email@example.com')
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-port
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = environ.get('EMAIL_PORT', 587)
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-subject-prefix
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#email-use-tls
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
 EMAIL_USE_TLS = True
 
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#server-email
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = EMAIL_HOST_USER
 ########## END EMAIL CONFIGURATION
 
@@ -43,7 +43,7 @@ DATABASES = postgresify()
 
 
 ########## CACHE CONFIGURATION
-# See: https://docs.djangoproject.com/en/1.3/ref/settings/#caches
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = memcacheify()
 ########## END CACHE CONFIGURATION
 
